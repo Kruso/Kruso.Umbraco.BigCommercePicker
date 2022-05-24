f7d7a8dbb2ea3cf0db65ce205ce702da17b564d9
Erik Henningson <erik@kruso.se>
2022-05-24 14:47:07 +0200

    Targeting Umbraco 10/.Net6

--- Kruso.Umbraco.BigCommercePicker/Editors/BigCommercePickerConfigurationEditor.cs
+++ Kruso.Umbraco.BigCommercePicker/Editors/BigCommercePickerConfigurationEditor.cs
@@ -5,9 +5,6 @@ namespace Kruso.Umbraco.BigCommercePicker.Editors
 {
     public class BigCommercePickerConfigurationEditor : ConfigurationEditor<BigCommercePickerConfiguration>
     {
-        //public BigCommercePickerConfigurationEditor(IIOHelper ioHelper) : base(ioHelper)
-        //{
-        //}
         public BigCommercePickerConfigurationEditor(IIOHelper ioHelper) : base(ioHelper)
         {
         }
